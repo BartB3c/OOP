1c8186aeb972557537acf61449181849bf4df72e
BarB3c <bartek02beczek@wp.pl>
2023-03-09 18:58:25 +0100

    zmiana

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        new Polygon(5, new Style("Black", "White", 2.5));
         Style style = new Style("red", "black", 2.0);
         Polygon rectangle = new Polygon(4, style);
-        Polygon triangle = new Polygon(3, new Style("black", "blue", 0.0));
+        Polygon triangle = new Polygon(3, style);
         Ellipse ellipse = new Ellipse(new Point(5.0, 5.0), 5.0, 10.0, style);
         rectangle.setVertices(new Point[]{new Point(0.0, 0.0), new Point(0.0, 100.0), new Point(100.0, 100.0), new Point(100.0, 0.0)});
         triangle.setVertices(new Point[]{new Point(200.0, 200.0), new Point(200.0, 300.0), new Point(100.0, 100.0)});
